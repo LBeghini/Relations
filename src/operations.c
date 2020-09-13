bb1cc575519f219b91aff7f8952b7f2db22f9158
Lavínia Beghini <lavinia.beghini@gmail.com>
2020-09-12 22:11:14 -0300

    Implement square root of

--- src/operations.c
+++ src/operations.c
@@ -3,6 +3,7 @@
 //
 
 #include "operations.h"
+#include <math.h>
 
 Couple *greater_than(Node *Ahead, Node *Bhead) {
     Couple * result = (Couple *)calloc(1, sizeof(Couple));
@@ -123,4 +124,33 @@ Couple *square_of(Node *Ahead, Node *Bhead){
     return result;
 
 }
+
+Couple *square_root_of(Node *Ahead, Node *Bhead){
+    Couple * result = (Couple *)calloc(1, sizeof(Couple));
+    Couple * current = result;
+    Node * A = Ahead;
+    Node * B = Bhead;
+
+    while (A != NULL) {
+        while (B != NULL){
+            if((double)A->value == sqrt(B->value)){
+                if(!current->y || !current->x){
+                    current->x = A->value;
+                    current->y = B->value;
+                    B = B->next;
+                    continue;
+                }
+                current->next = (Couple *) calloc(1, sizeof(Couple));
+                current = current->next;
+                current->x = A->value;
+                current->y = B->value;
+            }
+            B = B->next;
+        }
+        B = Bhead;
+        A = A->next;
+    }
+
+    return result;
+
 }
